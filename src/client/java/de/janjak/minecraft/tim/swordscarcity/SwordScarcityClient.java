b591b47f27ad791be72092b1c8f6371576ccbb64
Jan Erik Karuc <jan.karuc@rwth-aachen.de>
2025-05-10 15:38:34 +0200

    Adding template from https://fabricmc.net/develop/template/

---
+++ src/client/java/de/janjak/minecraft/tim/swordscarcity/SwordScarcityClient.java
@@ -0,0 +1,10 @@
+package de.janjak.minecraft.tim.swordscarcity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SwordScarcityClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
